403ad9e3a688c20f9a0abeb793a394b402a9089c
Dayton <daytonforehand@live.com>
2023-02-07 01:51:29 -0600

    Cleaned up variable names. Added a couple more columns to assignment table and updated query to reflect changes

--- api.go
+++ api.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -11,9 +12,11 @@ import (
 	"github.com/joho/godotenv"
 )
 
-type Spec struct {
+type AssigmentTableQuery struct {
 	Input_file  string `json:"input_file"`
 	Output_file string `json:"output_file"`
+	Language    string `json:"language"`
+	Args        string `json:"args"`
 }
 
 func Server() {
@@ -25,7 +28,7 @@ func Server() {
 			// calling this function parses the request body and fills the req.MultipartForm field
 			// err := req.ParseMultipartForm(32 << 20) // 32 << 20 = 32 MB for max memory to hold the files
 			// if err != nil {
-			// 	wWriteHeader(http.StatusBadRequest + 21)
+			// 	w.WriteHeader(http.StatusBadRequest + 21)
 			// 	return
 			// }
 
@@ -36,17 +39,22 @@ func Server() {
 				fmt.Printf("Assigment ID is: %s\n", assignmentId)
 			} else {
 				fmt.Println("No assignment ID is passed")
+				w.WriteHeader(http.StatusBadRequest + 20)
+				return
 			}
 
 			// Load environment Variables
 			err := godotenv.Load(".env")
 			util.Throw(err)
 			supabaseKey := os.Getenv("SUPABASE_SERVICE_ROLE_KEY")
-			
 
 			// Get spec file based on assignment ID
 			// Initialize request object
-			req, err := http.NewRequest("GET", "https://kasxttiggmakvprevgrp.supabase.co/rest/v1/assignment?select=input_file,output_file&id=eq."+assignmentId, nil)
+			req, err := http.NewRequest(
+				"GET",
+				"https://kasxttiggmakvprevgrp.supabase.co/rest/v1/assignment?select=input_file,output_file,language,args&id=eq."+assignmentId,
+				nil,
+			)
 			util.Throw(err)
 			req.Header.Add("apikey", supabaseKey)
 			req.Header.Add("Authorization", "Bearer "+supabaseKey)
@@ -69,11 +77,33 @@ func Server() {
 			// bodyString := string(bodyBytes)
 
 			// Decode JSON in a confusing way
-			var specificationFiles []Spec
-			err = json.NewDecoder(resp.Body).Decode(&specificationFiles)
+			var queryResults []AssigmentTableQuery
+			err = json.NewDecoder(resp.Body).Decode(&queryResults)
 			util.Throw(err)
 
-			fmt.Fprint(w, specificationFiles[0])
+			// Get spec file contents located at the links retrieved from supabase
+			inFileResp, err := http.Get(queryResults[0].Input_file)
+			util.Throw(err)
+			inFileByteContent, err := io.ReadAll(inFileResp.Body)
+			defer inFileResp.Body.Close()
+			util.Throw(err)
+
+			outFileResp, err := http.Get(queryResults[0].Output_file)
+			util.Throw(err)
+			outFileByteContent, err := io.ReadAll(outFileResp.Body)
+			defer outFileResp.Body.Close()
+			util.Throw(err)
+
+			// Convert to strings as originally in []byte form, each byte being a unicode numeric representatin of each character in the string
+			inFileContent := string(inFileByteContent)
+			outFileContent := string(outFileByteContent)
+
+			fmt.Printf("Input File Content: %s\n\nOutput File Content: %s\n", inFileContent, outFileContent)
+			fmt.Printf("Language: %s, Args: %s\n", queryResults[0].Language, queryResults[0].Args)
+
+			//
+
+			fmt.Fprint(w, "Still POSTed up! o7")
 
 		default:
 			w.WriteHeader(http.StatusMethodNotAllowed)
--- go.mod
+++ go.mod
@@ -5,13 +5,19 @@ go 1.17
 require (
 	github.com/andreyvit/diff v0.0.0-20170406064948-c7f18ee00883
 	github.com/joho/godotenv v1.5.1
+<<<<<<< HEAD
 	github.com/nedpals/supabase-go v0.2.0
+=======
+>>>>>>> dd79660 (Cleaned up variable names. Added a couple more columns to assignment table and updated query to reflect changes)
 	github.com/vulpine-io/io-test v1.0.0
 )
 
 require (
+<<<<<<< HEAD
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/nedpals/postgrest-go v0.1.3 // indirect
+=======
+>>>>>>> dd79660 (Cleaned up variable names. Added a couple more columns to assignment table and updated query to reflect changes)
 	github.com/sergi/go-diff v1.2.0 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
