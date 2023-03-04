4c74248a3483454d581a4778b4abb310dda8f852
Forehand, Dayton <daytonforehand@my.unt.edu>
2023-03-04 14:30:51 -0600

    Modified SubmissionResult and AssignmentInfo structs, added new Test struct, and moved helper func to util

--- util/main.go
+++ util/main.go
@@ -2,7 +2,9 @@ package util
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
+	"strings"
 )
 
 // Collection of submission results. Includes an order array to indicate the order of items in the
@@ -14,25 +16,36 @@ type SubmissionResults struct {
 
 // Record of a student's submission, with metadata about how it ran and compiled.
 type SubmissionResult struct {
-	Student        string
-	CompileSuccess bool
-	RunCorrect     bool
-	Feedback       string
-	AssignmentId   int8
-	StudentId      int8
+	Student        	string
+	CompileSuccess 	bool
+	Score						int8
+	Feedback       	[]string
+	AssignmentId   	int8
+	StudentId      	int8
+}
+
+type Test struct {
+	Expected 	string 	`json:"Expected"`
+	Input 		string	`json:"Input"`
+	Weight 		int8		`json:"Weight"`
 }
 
 type AssignmentInfo struct {
-	AssignmentId int8
+	AssignmentId 	int8		`json:"AssignmentId"`
+	Tests 				[]Test	`json:"Tests"`
 }
 
-func CalculateScore(result SubmissionResult) (score int) {
-	if result.CompileSuccess {
-		score += 50
+func CalculateScore(result SubmissionResult, tests []Test) (score int) {
+	var scorePossible int = 0
+	var scoreEarned int = 0
+	for i, feedback := range result.Feedback {
+		scorePossible += int(tests[i].Weight)
+		if feedback == "" {
+			scoreEarned += int(tests[i].Weight)
+		}
 	}
-	if result.RunCorrect {
-		score += 50
-	}
+
+	score = int((float64(scoreEarned) / float64(scorePossible)) * 100)
 	return
 }
 
@@ -61,4 +74,15 @@ func ParseOgInfo(path string) (info AssignmentInfo) {
 
 	json.Unmarshal(data, &info)
 	return
+}
+
+// Helper method to turn string slice into a readable, new line separated string that will print well in the report
+func StringSliceToPrettyString(input []string) string {
+	var output string = ""
+	for _, str := range input {
+		if str != "" {
+			output += fmt.Sprintf("%s\n", str)
+		}
+	}
+	return strings.TrimSpace(output)
 }
